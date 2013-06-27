ae8c44ea781241a9f6dd720633d5af03eb9ef7e0
Luzhow <luzhow@live.cl>
2013-06-27 11:36:27 -0400

    base de datos para pruebas

--- crearDB.py
+++ crearDB.py
@@ -12,14 +12,17 @@ def main():
 	create_peliculas(con)
 	populate_peliculas(con)
 	create_actor_has_peliculas(con)
+	populate_actor_has_peliculas(con)
+	con.commit()
 
 def conect():
 	con = sqlite3.connect('main_database.db')
+	con.text_factory = str
 	return con
 
 def create_actor(con):
 	c = con.cursor()
-	query = """CREATE TABLE actor(id_actor PRIMARY KEY int, 
+	query = """CREATE TABLE actor(id_actor integer PRIMARY KEY AUTOINCREMENT,
 									nombre text, 
 									birthday date, 
 									genero text, 
@@ -27,23 +30,41 @@ def create_actor(con):
 	c.execute(query)
 
 def populate_actor(con):
-	#Llenar tabla actor
+	c = con.cursor()
+	query = """INSERT INTO actor(nombre,birthday,genero,imagen) VALUES (?,?,?,?)"""
+	for i in info.actores:
+		c.execute(query,i)
 
 def create_peliculas(con):
 	c = con.cursor()
-	query = """CREATE TABLE peliculas(id_pelicula PRIMARY KEY int,
+	query = """CREATE TABLE peliculas(id_pelicula integer PRIMARY KEY AUTOINCREMENT,
 										nombre text, 
 										estreno date, 
 										pais text, 
-										descripcion text""")
+										descripcion text)"""
 	c.execute(query)
 
 def populate_peliculas(con):
-	#Llenar tabla peliculas
+	c = con.cursor()
+	query = """INSERT INTO peliculas(nombre,estreno,pais,descripcion) VALUES (?,?,?,?)"""
+	for i in info.peliculas:
+		c.execute(query,i)
 
 def create_actor_has_peliculas(con):
-	#crear tabla actor_has_peliculas
+	c = con.cursor()
+	query = """CREATE TABLE actor_has_peliculas(fk_id_actor int,
+												fk_id_pelicula int,
+												personaje text,
+												descripcion_rol text,
+												FOREIGN KEY(fk_id_pelicula) REFERENCES peliculas(id_pelicula),
+												FOREIGN KEY(fk_id_actor) REFERENCES actor(id_actor))"""
+	c.execute(query)
 
+def populate_actor_has_peliculas(con):
+	c = con.cursor()
+	query = """INSERT INTO actor_has_peliculas VALUES (?,?,?,?)"""
+	for i in info.relaciones:
+		c.execute(query,i)
 
 if __name__ == '__main__':
 	main()
--- info.py
+++ info.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #Info para main_database.db
+import datetime
 
-actor = ["nombre", "edad", "cumpleaños", "lol"]
+actores = [["nombre1", datetime.date(2003,6,25), "genero", "imagen"],
+		   ["nombre2", datetime.date(2003,6,25), "descripción2", "imagen"]]
+
+peliculas = [["nombre1", datetime.date(2003,6,25), "genero", "imagen"],
+		     ["nombre2", datetime.date(2003,6,25), "descripción2", "imagen"]]
+
+relaciones = [[1,1,"nombre","descripcion"],
+			  [2,2,"nombre","descripcion"]]
