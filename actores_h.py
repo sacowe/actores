1f99d4c2efddae3de146923aa00e3c52de8dc1c5
Luzhow <luzhow@live.cl>
2013-07-14 21:58:49 -0400

    eliminados algunos archivos sin uso

--- actores_h.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'actores_h.ui'
-#
-# Created: Thu Jul  4 12:11:25 2013
-#      by: pyside-uic 0.2.13 running on PySide 1.1.1
-#
-# WARNING! All changes made in this file will be lost!
-
--- w_peliculas.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'w_peliculas.ui'
-#
-# Created: Thu Jul  4 12:33:04 2013
-#      by: pyside-uic 0.2.13 running on PySide 1.1.1
-#
-# WARNING! All changes made in this file will be lost!
-
